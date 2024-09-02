59956103e104ea5b69b38da8490c99784d1da514
BardoJos <145452465+BardoJos@users.noreply.github.com>
2024-09-02 13:51:45 +0200

    Update Command.kt

--- app/src/main/java/com/farrusco/ics2000/Command.kt
+++ app/src/main/java/com/farrusco/ics2000/Command.kt
@@ -1,10 +1,8 @@
 
-import java.nio.ByteBuffer
-
 class Command {
 
     private val _header = ByteArray(43)
-    private var _data = ByteArray(0)
+    private var _data = ""
 
     init {
         setFrame(1)
@@ -25,42 +23,25 @@ class Command {
     fun setMac(mac: String) {
         val arr = mac.replace(":", "").chunked(2).map { it.toInt(16).toByte() }.toByteArray()
         if (arr.size == 6) {
-            insertbytes(_header, arr, 3)
+            Bytes.insertBytes(_header, arr, 3)
         }
     }
 
     fun setMagic() {
         val num = 653213
-        insertint32(_header, num, 9)
+        Bytes.insertInt32(_header, num, 9)
     }
 
     fun setEntityId(entityid: Int) {
-        insertint32(_header, entityid, 29)
+        Bytes.insertInt32(_header, entityid, 29)
     }
 
-    fun setData(data: ByteArray, aes: String) {
-        _data = Crypto.encrypt(data.toString(), aes)
+    fun setData(data: String, aes: String) {
+        _data = Crypto.encrypt(data, aes)
     }
 
     fun getCommand(): String {
-        insertint16(_header, _data.size, 41)
-        return _header.joinToString("") { String.format("%02x", it) } + _data.joinToString("") { String.format("%02x", it) }
+        Bytes.insertInt16(_header, _data.length/2, 41)
+        return _header.joinToString("") { String.format("%02x", it) } + _data
     }
-
-    private fun insertbytes(header: ByteArray, bytes: ByteArray, offset: Int) {
-        System.arraycopy(bytes, 0, header, offset, bytes.size)
-    }
-
-    private fun insertint32(header: ByteArray, value: Int, offset: Int) {
-        val buffer = ByteBuffer.allocate(4).putInt(value)
-        System.arraycopy(buffer.array(), 0, header, offset, 4)
-    }
-
-    private fun insertint16(header: ByteArray, value: Int, offset: Int) {
-        val buffer = ByteBuffer.allocate(2).putShort(value.toShort())
-        System.arraycopy(buffer.array(), 0, header, offset, 2)
-    }
-
 }
-
-
