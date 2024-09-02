741a4b17de1f0b8fc6b26764ddb1b2a1ccc22e15
BardoJos <145452465+BardoJos@users.noreply.github.com>
2024-09-02 13:51:05 +0200

    Update Bytes.kt

--- app/src/main/java/com/farrusco/ics2000/Bytes.kt
+++ app/src/main/java/com/farrusco/ics2000/Bytes.kt
@@ -1,30 +1,70 @@
 
 import kotlin.math.pow
 
-val MAX_UINT_16 = (2.0).pow(16.0) - 1.0
 
-fun insertInt32(arr: ByteArray, num: Int, start: Int) {
-    arr[start] = (num and 0xFF).toByte()
-    arr[start + 1] = (num shr 8 and 0xFF).toByte()
-    arr[start + 2] = (num shr 16 and 0xFF).toByte()
-    arr[start + 3] = (num shr 24 and 0xFF).toByte()
-}
+object Bytes {
 
-fun insertInt16(arr: ByteArray, num: Int, start: Int) {
-    arr[start] = (num and 0xFF).toByte()
-    arr[start + 1] = (num shr 8 and 0xFF).toByte()
-}
+    val MAX_UINT_16 = (2.0).pow(16.0) - 1.0
+    val MAX_VALUE_U_INT_32 = 2.0.pow(32.0).toLong() - 1L
 
-fun insertBytes(arr: ByteArray, inp: ByteArray, start: Int) {
-    for (i in inp.indices) {
-        arr[i + start] = inp[i]
+    fun insertInt32(arr: ByteArray, num: Int, start: Int) {
+        arr[start] = (num and 0xFF).toByte()
+        arr[start + 1] = (num shr 8 and 0xFF).toByte()
+        arr[start + 2] = (num shr 16 and 0xFF).toByte()
+        arr[start + 3] = (num shr 24 and 0xFF).toByte()
     }
-}
 
-fun byteToInt2(byte1: Byte, byte2: Byte): Int {
-    return (byte1.toInt() and 0xFF or (byte2.toInt() and 0xFF shl 8)) and 0xFFFF
-}
+    fun insertInt16(arr: ByteArray, num: Int, start: Int) {
+        arr[start] = (num and 0xFF).toByte()
+        arr[start + 1] = (num shr 8 and 0xFF).toByte()
+    }
 
-fun byteToInt4(byte1: Byte, byte2: Byte, byte3: Byte, byte4: Byte): Int {
-    return (byte1.toInt() and 0xFF or (byte2.toInt() and 0xFF shl 8) or (byte3.toInt() and 0xFF shl 16) or (byte4.toInt() and 0xFF shl 24)) and 0xFFFF
+    fun insertBytes(arr: ByteArray, inp: ByteArray, start: Int) {
+        for (i in inp.indices) {
+            arr[i + start] = inp[i]
+        }
+    }
+
+    fun byteToInt2(byte1: Byte, byte2: Byte): Int {
+        return (byte1.toInt() and 0xFF or (byte2.toInt() and 0xFF shl 8)) and 0xFFFF
+    }
+
+    fun byteToInt4(byte1: Byte, byte2: Byte, byte3: Byte, byte4: Byte): Int {
+        return (byte1.toInt() and 0xFF or (byte2.toInt() and 0xFF shl 8) or (byte3.toInt() and 0xFF shl 16) or (byte4.toInt() and 0xFF shl 24)) and 0xFFFF
+    }
+    fun hexToByteArray(s: String): ByteArray {
+        val length = s.length
+        val array = ByteArray(length / 2)
+        var n = 0
+        while (true) {
+            val n2 = n + 1
+            if (n2 >= length) {
+                break
+            }
+            array[n / 2] = ((s[n].digitToIntOrNull(16) ?: -1 shl 4) + s[n2].digitToIntOrNull(16)!!
+                ?: -1).toByte()
+            n += 2
+        }
+        return array
+    }
+    fun hexToByteArrayx(string: String): ByteArray {
+        val ar =  string.chunked(2).map{ it.toInt(16)  }
+        val ba = ByteArray(ar.size)
+        ar.forEachIndexed { index, i -> ba[index] = i.toByte() }
+        return ba
+        //return this.chunked(2).map { it.toInt(16).toByte() }.toByteArray()
+    }
+
+    fun byteArrayToHex(ba: ByteArray): String{
+        var rtn = ""
+        ba.forEach {
+            rtn +=  (0xff and it.toInt()).toString(16).padStart(2,'0')
+        }
+        return rtn
+    }
+
+    fun pad(s: String, blockSize:Int): String {
+        val padding = blockSize - s.length % blockSize
+        return s + (padding.toChar().toString().repeat(padding))
+    }
 }
